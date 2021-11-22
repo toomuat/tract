b1ecd66fc27745233dd724109d5c10a4a219066e
Mathieu Poumeyrol <kali@zoy.org>
2021-11-22 14:28:28 +0100

    fix "fake" bug

--- linalg/src/frame/mmm/scratch.rs
+++ linalg/src/frame/mmm/scratch.rs
@@ -216,6 +216,9 @@ impl<TI: Copy + Datum + Zero> ScratchSpaceFusedNonLinear<TI> {
                                     .get_unchecked(..have),
                             );
                         }
+                        if cfg!(debug_assertions) {
+                            buf.get_unchecked_mut(have..).iter_mut().for_each(|x| *x = TI::zero());
+                        }
                         buf.as_ptr()
                     } else {
                         v.as_ptr_unchecked::<TI>().add(down * K::mr())
@@ -240,6 +243,9 @@ impl<TI: Copy + Datum + Zero> ScratchSpaceFusedNonLinear<TI> {
                                     .get_unchecked(..have),
                             );
                         }
+                        if cfg!(debug_assertions) {
+                            buf.get_unchecked_mut(have..).iter_mut().for_each(|x| *x = TI::zero());
+                        }
                         buf.as_ptr()
                     } else {
                         v.as_ptr_unchecked::<TI>().add(right * K::nr())
@@ -263,6 +269,9 @@ impl<TI: Copy + Datum + Zero> ScratchSpaceFusedNonLinear<TI> {
                                 .get_unchecked(down * K::mr()..)
                                 .get_unchecked(..have),
                         );
+                        if cfg!(debug_assertions) {
+                            r.get_unchecked_mut(have..).iter_mut().for_each(|x| *x = TI::zero());
+                        }
                         r.as_ptr()
                     } else {
                         rows.as_ptr_unchecked::<TI>().add(down * K::mr())
@@ -279,6 +288,9 @@ impl<TI: Copy + Datum + Zero> ScratchSpaceFusedNonLinear<TI> {
                                 .get_unchecked(right * K::nr()..)
                                 .get_unchecked(..have),
                         );
+                        if cfg!(debug_assertions) {
+                            r.get_unchecked_mut(have..).iter_mut().for_each(|x| *x = TI::zero());
+                        }
                         c.as_ptr()
                     } else {
                         cols.as_ptr_unchecked::<TI>().add(right * K::nr())
